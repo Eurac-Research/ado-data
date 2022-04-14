a1c4cd6400f66de5e1dd0b3fb6e548eeb2a18ebe
felixgreifeneder <felix.greifeneder@gmail.com>
2022-04-14 15:40:32 +0000

    adds the "update" scripts and fixes some bugs

--- scripts/update_geojson.py
+++ scripts/update_geojson.py
@@ -21,20 +21,21 @@ def main():
     # set date range
     today = dt.datetime.strptime('2022-03-23',
                                  '%Y-%m-%d')  # TODO: modify to use actual date of the day the script is run
-    drange = pd.date_range(today - dt.timedelta(days=364), today, freq='1D')
-
-    today = dt.datetime.strptime('2020-08-01',
-                                 '%Y-%m-%d')
-    drange2 = pd.date_range(today - dt.timedelta(days=364), today, freq='1D')
 
     def call_compute_stats(index):
-        usedrange = drange2 if index in ['VHI', 'VCI', 'CDI'] else drange
-        tools.compute_stats(usedrange, nuts3, index=index, aggunit='nuts')
-        # tools.update_metadata_drange(index, usedrange)
+        # determine the last date in the index geojson
+        tmp_geojson = gpd.read_file('../json/nuts/' + index + '-latest.geojson')
+        tmp_series = pd.DataFrame.from_dict(tmp_geojson[index][0], orient='index', columns=['droughtindex'])
+        maxdate = tmp_series.index.max()
+        drange = pd.date_range(maxdate + dt.timedelta(days=1), today, freq='1D')
+        total_drange = pd.date_range(today - dt.timedelta(days=364), today, freq='1D')
+
+        tools.compute_stats(drange, tmp_geojson, index=index, aggunit='nuts', update=True)
+        tools.update_metadata_drange(index, total_drange)
 
     Parallel(n_jobs=-1)(delayed(call_compute_stats)(i) for i in index_list)
 
-    tools.merge_time_series(nuts3, index_list, aggunit='nuts')
+    tools.merge_time_series(nuts3, index_list, aggunit='nuts', update=True)
 
 
 if __name__ == "__main__":
